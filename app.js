ea81e9174eb7c6560498e57157fcccdbc84abe89
enghamzasalem <enghamzasalem@yahoo.com>
2020-07-17 00:28:25 +0300

    add basic files

---
+++ app.js
@@ -0,0 +1,31 @@
+const container = document.querySelector(".container")
+const coffees = [
+  { name: "Perspiciatis", image: "AAAsset.jpg" },
+  { name: "Voluptatem",image: "AAAsset.jpg"  },
+  { name: "Explicabo", image: "AAAsset.jpg"  },
+ 
+]
+const showCoffees = () => {
+  let output = ""
+  coffees.forEach(
+    ({ name, image }) =>
+      (output += `
+              <div class="card">
+                <img class="card--avatar" src=${image} />
+                <h1 class="card--title">${name}</h1>
+                <a class="card--link" href="#">Taste</a>
+              </div>
+              `)
+  )
+  container.innerHTML = output
+}
+
+document.addEventListener("DOMContentLoaded", showCoffees)
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
